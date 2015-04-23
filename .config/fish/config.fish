6c4732f61d0902ee8b4284252fd928980b7015bb
Lee Fan <leefan@leefan.com>
2015-04-23 13:43:39 -0700

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
-# Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 set TERM xterm-256color
+set HOST (hostname)
 
 # Theme
 set fish_theme bobthefish
@@ -12,8 +12,19 @@ set -g theme_display_user yes
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
 set fish_plugins git brew ssh vi-mode
+
+function title
+    printf "\033k$argv\033\\"
+end
+
+# sets user@hostname for screen and tmux title
+if [ -z "$STY" ]
+    title "$USER@$HOST"
+else if [ -n "$TMUX" ]
+    title "$USER@$HOST"
+end
+
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
