298a477c247977dccf61e1bbeb636680950e868e
Lee Fan <leefan09@gmail.com>
2015-10-14 14:32:51 -0700

    fixed interactive shell issues

--- config.fish
+++ config.fish
@@ -3,36 +3,8 @@ set PATH /usr/local/bin /usr/local/sbin $PATH
 set TERM xterm-256color
 set HOST (hostname)
 
-# Theme
-set -g theme_display_git yes
-set -g theme_display_hg no
-set -g theme_display_virtualenv no
-set -g theme_display_ruby no
-set -g theme_display_user yes
-
 function title
-    printf "\033k$argv\033\\"
-end
-
-# sets user@hostname for screen and tmux title
-if [ -n "$STY" ]
-else if [ -n "$TMUX" ]
-else
-    title "$USER@$HOST"
-end
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-source $fish_path/oh-my-fish.fish
-
-# Theme and Plugins
-Theme "bobthefish"
-Plugin "ssh"
-
-if [ -z "$SSH_TTY" ]
-    tmux
+    printf "\033k$argv\033\\" # "
 end
 
 # vi-mode
@@ -50,4 +22,35 @@ function fish_mode_prompt --description "Displays the current mode"
         echo -n ''
     end
 end
-fish_vi_mode
+
+if status --is-login
+    # Theme
+    set -g theme_display_git yes
+    set -g theme_display_hg no
+    set -g theme_display_virtualenv no
+    set -g theme_display_ruby no
+    set -g theme_display_user yes
+
+    # sets user@hostname for screen and tmux title
+    if [ -n "$STY" ]
+    else if [ -n "$TMUX" ]
+    else
+        title "$USER@$HOST"
+    end
+
+    # Path to your custom folder (default path is $FISH/custom)
+    #set fish_custom $HOME/dotfiles/oh-my-fish
+
+    # Load oh-my-fish configuration.
+    source $fish_path/oh-my-fish.fish
+
+    # Theme and Plugins
+    Theme "bobthefish"
+    Plugin "ssh"
+
+    if [ -z "$SSH_TTY" ]
+        tmux
+    end
+    fish_vi_mode
+end
+
