c1d8d89444e88cf41a027f45d8f6255d3bd2d08a
Lee Fan <leefan09@gmail.com>
2017-03-13 10:46:39 -0700

    removed custom vi mode indicator

--- init.fish
+++ init.fish
@@ -7,20 +7,20 @@ function title
 end
 
 # vi-mode
-function fish_mode_prompt --description "Displays the current mode"
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        switch $fish_bind_mode
-        case default
-            printf "\e[1;38;5;%d;48;5;%dm N \e[0m" 0 144
-        case insert
-            printf "\e[1;38;5;%d;48;5;%dm I \e[0m" 0 45
-        case visual
-            printf "\e[1;38;5;%d;48;5;%dm V \e[0m" 0 208
-        end
-        set_color normal
-        echo -n ''
-    end
-end
+# function fish_mode_prompt --description "Displays the current mode"
+#     if test "$fish_key_bindings" = "fish_vi_key_bindings"
+#         switch $fish_bind_mode
+#         case default
+#             printf "\e[1;38;5;%d;48;5;%dm N \e[0m" 0 144
+#         case insert
+#             printf "\e[1;38;5;%d;48;5;%dm I \e[0m" 0 45
+#         case visual
+#             printf "\e[1;38;5;%d;48;5;%dm V \e[0m" 0 208
+#         end
+#         set_color normal
+#         echo -n ''
+#     end
+# end
 
 if status --is-login
     # Theme
