678d4e9db641194a87f36c0dd62bceba6c73908f
Lee Fan <leefan09@gmail.com>
2018-06-15 12:07:57 -0700

    sumologic specific config

---
+++ sumo.fish
@@ -0,0 +1,10 @@
+# aliases
+alias dsh=/Users/lee/git/sumo/ops/bin/dsh.sh
+alias props=/Users/lee/git/sumo/system/bin/local-props-updater.py
+
+# variables
+set -x SUMO_HOME /Users/lee/git/sumo
+set -x _JAVA_OPTIONS "-Djava.awt.headless=true"
+ulimit -n 65536
+set -x MVN_HOME /usr/local/Cellar/maven/3.5.0
+set -x MAVEN_OPTS "-Xmx1024m -Djava.awt.headless=true"
